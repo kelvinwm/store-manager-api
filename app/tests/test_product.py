a92db3a446a6a64c8e9b83f66cb60bd951b7311f
Prince Kelvin <kelvinwmblogs@gmail.com>
2018-10-17 22:27:40 +0300

    [#161296726] A test for add products which fails

--- app/tests/test_product.py
+++ app/tests/test_product.py
@@ -24,3 +24,10 @@ class TestProducts(unittest.TestCase):
         """TEST API can return all products in the list"""
         response = self.app.get('/api/v1/users/products', content_type='application/json')
         self.assertEqual(response.status_code, 200)
+
+    def test_add_product(self):
+        """TEST API can add product to list properly"""
+        response = self.app.post('/api/v1/users/products', data=json.dumps(self.data), content_type="application/json")
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(result["Message"], "Product added successfully")
+        self.assertEqual(response.status_code, 201)
