3c642e91a0132a2584f8378db8bf012ef4f57857
Prince Kelvin <kelvinwmblogs@gmail.com>
2018-10-20 12:44:10 +0300

    update config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-from instance.config import app_configuration
+# from instance.config import app_configuration
 
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_configuration['development'])
-    app.config.from_pyfile('config.py')
+    app = Flask(__name__)
+    # app.config.from_object(app_configuration['development'])
+    # app.config.from_pyfile('config.py')
     from app.api.v1 import admin_api
     app.register_blueprint(admin_api, url_prefix="/api/v1/users")
 
